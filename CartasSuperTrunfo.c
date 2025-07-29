737ca3a9b981d9ead90f3d7cd64de42b8c4055ae
MicaelGularte <micaelgusilva@outlook.com>
2025-07-29 13:56:01 +0000

    Criei struct, printf e scanf e variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 
-struct Carta{
-    char codigo[4];         //Código da carta
-    int populacao;          //População
-    float area;             //Área
-    float pib;              //PIB
-    int pontos_turisticos;  //Número de pontos turísticos
-    };
-
+struct Carta {
+    char codigo[4];
+    char estado[20];
+    char nome_da_cidade[20];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+};
     int main() {
         struct Carta carta1, carta2;        
 
-        //Entrada de dados para a carta 1
+        //Entrada das caracteristicas para a carta 1
         printf("Digite o código da carta 1: ");
-        scanf("%S", carta1.populacao);
+        scanf("%s", carta1.codigo);
+
+        printf("Digite o Estado: ");
+        scanf(" %19[^\n]", &carta1.estado);
+
+        printf("Digite o Nome da Cidade: ");
+        scanf(" %19[^\n]", &carta1.nome_da_cidade);
 
         printf("Digite a população da cidade %s: ", carta1.codigo);
         scanf("%d", &carta1.populacao);
@@ -22,11 +29,20 @@ struct Carta{
         scanf("%f", &carta1.area);
 
         printf("Digite o PIB da cidade %s: ", carta1.codigo);
+        scanf("%f", &carta1.pib);
+
+        printf("Digite o numero de pontos turisticos da cidade %s: ", carta1.codigo);
         scanf("%d", &carta1.pontos_turisticos);
 
-        //Entrada de dados para a carta 2
+        //Entrada das caracteristicas para a carta 2
         printf("\nDigite o código da carta 2: ");
         scanf("%s", carta2.codigo);
+
+        printf("Digite o Estado: ", carta2.estado);
+        scanf(" %c", &carta2.estado);
+
+        printf("Digite o Nome da Cidade: ", carta2.nome_da_cidade);
+        scanf(" %49[^\n]", &carta2.nome_da_cidade);
 
         printf("Digite a população da cidade %s: ", carta2.codigo);
         scanf("%d", &carta2.populacao);
@@ -43,6 +59,8 @@ struct Carta{
         //Exibição das caracteristicas das cartas
         printf("\n--- Dados da Carta 1 ---\n");
         printf("Código: %s\n", carta1.codigo);
+        printf("Estado: %c\n", carta1.estado);
+        printf("Cidade: %s\n", carta1.nome_da_cidade);
         printf("População: %d\n", carta1.populacao);
         printf("Área: %.2f\n", carta1.area);
         printf("PIB: %.2f\n", carta1.pib);
@@ -50,6 +68,8 @@ struct Carta{
 
         printf("\n--- Dados da Carta 2 ---\n");
         printf("Código: %s\n", carta2.codigo);
+        printf("Estado: %c\n", carta2.estado);
+        printf("Cidade: %s\n", carta2.nome_da_cidade);
         printf("População: %d\n", carta2.populacao);
         printf("Área: %.2f\n", carta2.area);
         printf("PIB: %.2f\n", carta2.pib);
